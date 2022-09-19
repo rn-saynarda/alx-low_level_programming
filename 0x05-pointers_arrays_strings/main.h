aa85fbfafaec416834a079c996322014694df01f
rn-saynarda <nigeltendaijnr@gmail.com>
2022-09-19 11:54:11 -0700

    update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
-int _putchar(char c); 
- void reset_to_98(int *n); 
+void reset_to_98(int *n); 
  void swap_int(int *a, int *b); 
  int _strlen(char *s); 
  void _puts(char *str); 
@@ -9,4 +8,5 @@ int _putchar(char c);
  void puts_half(char *str); 
  void print_array(int *a, int n); 
  char *_strcpy(char *dest, char *src); 
+ int _putchar(char c); 
  int _atoi(char *s);
