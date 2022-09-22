de0e5efad8caf73cc24ad32e069466b7db1ccd1b
rn-saynarda <nigeltendaijnr@gmail.com>
2022-09-22 10:55:12 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,22 @@
-#include <unistd.h>  
-
+#include <stdio.h>
 
 /**
-*main - Prints a text
-*
-*Return: Always 1 (Success)
+ * main - main function
+ * Return: end program
 */
+
 int main(void)
+
 {
-  write(2,"and that piece of art is useful\"-Dora Korpar,
-2015-10-19\n",59);
-   return (1);
+
+			char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+
+
+			fprintf(stderr, "%s\n", n);
+
+
+
+			return (1);
+
 }
