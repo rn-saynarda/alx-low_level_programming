97ee8224216e682b3ae5d604c346cc85137dd658
rn-saynarda <nigeltendaijnr@gmail.com>
2022-09-22 07:20:16 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,13 @@
 /**
 *main - print the string in the put function
 *
-*Description: using the main function 
+*Description:using the main function*
 *This program prints "Programming is like building a multilingual puzzle
 *Return: 0
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-Return (0);
-} 
+printf('with proper grammar, but the outcome is a piece of art,\n');
+Return(0);
+}
 
