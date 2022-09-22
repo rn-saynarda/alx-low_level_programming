098bde7bf5d56093d91a2068f522d99a9e2ae6a1
rn-saynarda <nigeltendaijnr@gmail.com>
2022-09-22 09:21:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.h> 
-  
- /** 
-  * main -Programming is like building a multilingual 
-  * 
-  * Return: Output 0 
-  */ 
- int main(void) 
- { 
- puts("\"Programming is like building a multilingual puzzle"); 
- return (0); 
- }
+#include <stdio.h>
+/**
+*main-Programming is like building a multilingual
+*
+*Return: Output 0
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
